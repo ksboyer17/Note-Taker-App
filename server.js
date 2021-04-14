226c45d9763e5827b151a8d612e9f2007de15d55
Kenny Boyer <ksboyerii@gmail.com>
2021-04-14 15:58:42 -0500

    added to serverjs

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+
+const handleRequest = (req, res) => {
+    // Saving the request data as a variable
+    let requestData = '';
+
+
+
+
+
+    // Create the server, assign it to a variable called "server"
+const server = http.createServer(handleRequest);
+
+// Start our server
+server.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
