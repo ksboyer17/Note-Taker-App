10053c3f03a54f03485e98d427bd3115fe9f2e07
Kenny Boyer <ksboyerii@gmail.com>
2021-04-14 16:00:42 -0500

    added to serverjs

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const handleRequest = (req, res) => {
     // Saving the request data as a variable
     let requestData = '';
 
+     // When the server receives data...
+  req.on('data', (data) => {
+    // Add it to requestData.
+    requestData += data;
+  });
+
+  // When the request has ended...
+  req.on('end', () => {
+    // Log (server-side) the request method, as well as the data received!
+    console.log(`You did a ${req.method}, with the data:\n, ${requestData}`);
+    res.end();
+  });
+};
+
 
 
 
